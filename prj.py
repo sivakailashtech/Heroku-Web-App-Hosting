50806f6ee668743d651730efd8ba91bf30fcf466
A. Siva Kailash <sivakailashtech@gmail.com>
2022-12-07 23:43:36 +0530

    Add files via upload

---
+++ prj.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("TDS Graded Assignment 8")
+st.subheader("odd even checking Web App')
+placeholder_Number = st.empty()
+n = placeholder_Number.number_input('Enter Number')
+if (n % 2) = 0:
+	st.write("your number is even")
+else:
+	st.write("your number is odd")
