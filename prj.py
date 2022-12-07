c3dc7439f3a8ef1641d6c81d21802652318b48a0
A. Siva Kailash <sivakailashtech@gmail.com>
2022-12-07 23:54:11 +0530

    Update prj.py

--- prj.py
+++ prj.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.title("TDS Graded Assignment 8")
-st.subheader("odd even checking Web App")
+st.subheader("odd or even number checking Web App by Siva Kailash (21f1002374)")
 placeholder_Number = st.empty()
-n = placeholder_Number.number_input('Enter Number')
+int(n) = placeholder_Number.number_input('Enter Number')
 if (n % 2) == 0:
 	st.write("your number is even")
 else:
