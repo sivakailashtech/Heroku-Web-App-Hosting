68894af0eb1354735e6eec51ee96dd4a952339bf
A. Siva Kailash <sivakailashtech@gmail.com>
2022-12-07 23:49:44 +0530

    Update prj.py

--- prj.py
+++ prj.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("TDS Graded Assignment 8")
-st.subheader("odd even checking Web App')
+st.subheader("odd even checking Web App")
 placeholder_Number = st.empty()
 n = placeholder_Number.number_input('Enter Number')
 if (n % 2) = 0:
