48a72d919deeaa6700979b5c945a628e0e5aa024
A. Siva Kailash <sivakailashtech@gmail.com>
2022-12-07 23:50:38 +0530

    Update prj.py

--- prj.py
+++ prj.py
@@ -3,7 +3,7 @@ st.title("TDS Graded Assignment 8")
 st.subheader("odd even checking Web App")
 placeholder_Number = st.empty()
 n = placeholder_Number.number_input('Enter Number')
-if (n % 2) = 0:
+if (n % 2) == 0:
 	st.write("your number is even")
 else:
 	st.write("your number is odd")
